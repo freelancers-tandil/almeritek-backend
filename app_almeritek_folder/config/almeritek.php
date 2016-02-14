1546298a6c427cfee4f08629a731204d5aed7df0
Mauro J Giamberardino <mgiamberardino@tecnisys.com>
2016-02-14 18:05:16 -0300

    Cambios en acceso a servicios

--- app_almeritek_folder/config/almeritek.php
+++ app_almeritek_folder/config/almeritek.php
@@ -2,6 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+
 $config['authorization_enabled']=true;
 
 $config['authorization_role_session_var']='rol';
@@ -9,7 +10,43 @@ $config['authorization_role_session_var']='rol';
 $config['authorization_exceptions']['user']['login']['post']=true;
 $config['authorization_exceptions']['user']['logout']['get']=true;
 $config['authorization_exceptions']['user']['checklogin']['post']=true;
-$config['authorization']['user']['*']['*']=array('0');
-$config['authorization']['*']['*']['*']=array('*');
+
+/*
+*
+* Por defecto solo los administradores acceden a todo
+*
+*/
+$config['authorization']['*']['*']['*']=array('0');
+
+
+/*
+*
+* Autorizaciones para los tickets
+*
+*/
+$config['authorization']['ticket']['*']['*']=array('0','1');
+$config['authorization']['ticket']['*']['delete']=array('0');
+
+/*
+*
+* Autorizaciones para los clientes
+*
+*/
+$config['authorization']['cliente']['list']['*']=array('*');
+$config['authorization']['cliente']['*']['get']=array('*');
+
+/*
+*
+* Autorizaciones para los talleres
+*
+*/
+$config['authorization']['taller']['list']['*']=array('*');
+
+/*
+*
+* Autorizaciones para los usuarios
+*
+*/
+$config['authorization']['user']['list']['*']=array('*');
 
  ?>
